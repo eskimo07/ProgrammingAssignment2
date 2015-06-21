a745043cc7b998c85c77f1f2aa9664ba82560468
eskimo07 <eskimo07@ymail.com>
2015-06-21 11:50:49 +0200

    My functions for Assignment2 of Coursera R Programming
    Signed-off-by: eskimo07 <eskimo07@ymail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,50 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-### This is a first trial to commit
 
 ## Write a short comment describing this function
-### Actually this functions do nothing 
+ 
+### The first function will create a list of that functions 
+### in an own environment (different from Global Environment):
+###             "set" 
+###             "get" 
+###             "setimatrix" 
+###             "getimatrix"
+### and the objects: 
+###             "im" 
+###             "x"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        im <- NULL
+        set <- function(y) {
+                x <<- y
+                im <<- NULL
+        }
+        get <- function() x
+        setimatrix <- function(solve) im <<- solve 
+        getimatrix <- function() im   
+        list(set = set, get = get,
+             setimatrix = setimatrix,
+             getimatrix = getimatrix)
 }
 
 
 ## Write a short comment describing this function
+
+### This function looks after the cached inverse matrix, 
+### and returns "im" if it exists (and the function ends!)  
+### If there is nothing cached "im" will be created.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        im <- x$getimatrix() 
+        if(!is.null(im)) {
+                message("getting cached data")
+                return(im)
+        }
+        data <- x$get()
+        im <- solve(data, ...) 
+        x$setimatrix(im)  
+        im
 }
+
+
